e42447688c71a79d4fb9db77436be82729f41fcd
unknown <wttgash489@gmail.com>
2020-10-07 14:25:08 +0100

    Substantial progress through beginner course

---
+++ Mosh/Course1/MoshCSharpBasics/Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Classes
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var will = new Person();
+            will.FirstName = "Will";
+            will.LastName = "Gash";
+            will.Introduce();
+
+        }
+    }
+}
---
+++ Mosh/Course1/PrimitiveTypesAndExpressions/TypeConversion/TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string number = "1234";
+                // int i = (int)number; does not work, even explicitly 
+                // instead we must use the convert class
+
+                // this will throw an exception due to the fact that 1234 is too large a number to be stored as a byte
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte");
+            }
+
+
+            try
+            {
+                string str = "false";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("the value of str was not recognised as a valid boolean");
+            }
+            
+
+        }
+    }
+}
